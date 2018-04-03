31e85f425dc32ba1739032e308b6011f41073d0b
Sean Enck <enckse@gmail.com>
2018-04-03 14:09:17 -0400

    how to track auths

---
+++ manage/auths.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+LIB=/var/lib/radiucal/
+LAST=${LIB}last_audits
+LAST_TIME=""
+DATE=$(date +%Y-%m-%d)
+SRT_LINE=0
+if [ -e $LAST ]; then
+    SRT_LINE=$(cat $LAST)
+fi
+LOG=${LIB}log/radiucal.audit.$DATE
+if [ -e $LOG ]; then
+    lines=$(cat $LOG | wc -l)
+    cat $LOG | tail -n +$((SRT_LINE+1)) | uniq | smirc
+    echo "$lines" > $LAST
+fi
