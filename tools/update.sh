7567731b22e77243a270c4f475f91d78627006fb
Sean Enck <enckse@gmail.com>
2018-04-04 13:25:16 -0400

    update is open now

---
+++ tools/update.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+LOCAL_CONF=~/.config/epiphyte/env
+source /etc/environment
+IS_LOCAL=0
+if [ -e $LOCAL_CONF ]; then
+    source $LOCAL_CONF
+    IS_LOCAL=1
+fi
+echo "updating network configuration"
+if [ $IS_LOCAL -eq 0 ]; then
+    git reset --hard
+fi
+git pull
+
+for c in $(echo "users/"); do
+    cache="${c}__pycache__"
+    if [ -d $cache ]; then
+        echo "clearing cache: $cache"
+        rm -rf $cache
+    fi
+done
+
+BIN=bin/
+mkdir -p $BIN
+USERS=${BIN}eap_users
+PREV=${USERS}.prev
+if [ -e $USERS ]; then
+    cp $USERS $PREV
+fi
+
+radiucal-bootstrap
+if [ $? -ne 0 ]; then
+    echo "unable to bootstrap (radiucal-utils|tools installed?)"
+    exit 1
+fi
+python config_compose.py --output $PWD/$BIN
+if [ $? -ne 0 ]; then
+    echo "composition errors"
+    exit 1
+fi
+diffed=1
+if [ -e $USERS ]; then
+    if [ -e $PREV ]; then
+        changes=$(diff -u $PREV $USERS)
+        diffed=$?
+        if [ $diffed -ne 0 ]; then
+            if [ $IS_LOCAL -eq 1 ]; then
+                echo "changes" | grep -v '"pass":'
+                echo
+                echo "===INFO==="
+                echo "the above summarizes the network changes you are making"
+                echo "===INFO==="
+                echo
+            fi
+        fi
+    fi
+fi
+
+if [ $diffed -ne 0 ]; then
+    echo "network configuration updated"
+    if [ $IS_LOCAL -eq 0 ]; then
+        # run local reports
+        if [ -e "./reports" ]; then
+            ./reports
+        fi
+        radiucal-report
+    fi
+fi
