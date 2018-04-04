7c60234c55416f351e3a77530b7d21f6e82a0dbb
Sean Enck <enckse@gmail.com>
2018-04-04 14:44:42 -0400

    assignments

--- tools/reports.sh
+++ tools/reports.sh
@@ -8,4 +8,10 @@ MEMBERSHIP=${BIN}membership.md
 echo "| vlan | user |
 | ---  | --- |" > $MEMBERSHIP
 
-cat $AUDITS | sed "s/,/ /g" | awk '{print "| " toupper($2), "|", $1 " |"}' | sort -u >> $MEMBERSHIP
+cat $AUDITS | sed "s/,/ /g" | awk '{print "| " $2, "|", $1 " |"}' | sort -u >> $MEMBERSHIP
+
+ASSIGNED=${BIN}assigned.md
+echo "| user | vlan | mac |
+| --- | --- | --- |" > $ASSIGNED
+
+cat $AUDITS | sed "s/,/ | /g;s/^/| /g;s/$/ |/g" | sort -u >> $ASSIGNED
