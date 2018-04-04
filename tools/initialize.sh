f364ed190756dd36ac713e3da45c134b4cff1b23
Sean Enck <enckse@gmail.com>
2018-04-04 13:41:37 -0400

    how to full init

--- tools/initialize.sh
+++ tools/initialize.sh
@@ -4,6 +4,8 @@ if [ ! -d "$PWD/users" ]; then
     exit 1
 fi
 DIR=/usr/share/radiucal/
+cp $DIR/configure.sh configure
+chmod u+x configure
 for f in $(echo "config_compose new_user users/__config__ users/__init__"); do
     cp $DIR/$f.py $PWD/$f.py
 done
