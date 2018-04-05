225f0d197b36537a09a395a6f83fe8a43b92b820
Sean Enck <enckse@gmail.com>
2018-04-05 09:15:57 -0400

    clean up after self

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # monitor auths and send to smirc
 LIB=/var/lib/radiucal/
-LAST=${LIB}last_audits
 LAST_TIME=""
 DATE=$(date +%Y-%m-%d)
+AUDITS=${LIB}last_audits
+LAST=$AUDITS.$DATE
 SRT_LINE=0
 if [ -e $LAST ]; then
     SRT_LINE=$(cat $LAST)
+else
+    rm -f ${AUDITS}*
 fi
+echo "$SRT_LINE"
 LOG=${LIB}log/radiucal.audit.$DATE
 if [ -e $LOG ]; then
     lines=$(cat $LOG | wc -l)
-    cat $LOG | tail -n +$((SRT_LINE+1)) | cut -d " " -f 2- | uniq | smirc
+    cat $LOG | tail -n +$((SRT_LINE+1)) | cut -d " " -f 2- | uniq | smirc --private
+    cat $LOG | tail -n +$((SRT_LINE+1)) | grep "ERROR" | uniq | smirc 
     echo "$lines" > $LAST
 fi
