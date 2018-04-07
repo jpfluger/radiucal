74401f267b7a16bb4ff1863284faa4405cc22ff6
Sean Enck <enckse@gmail.com>
2018-04-07 19:43:53 -0400

    new test

---
+++ tools/tests/user_mab.py
@@ -0,0 +1,11 @@
+"""User with admin and dev and various macs."""
+import users.__config__ as __config__
+import users.common as common
+normal = __config__.Assignment()
+normal.macs = [common.VALID_MAC]
+normal.bypass = ["112233445567"]
+normal.vlan = "dev"
+normal.group = 'test'
+normal.password = 'ac0ae0d888d0e71c3dae227377a8601e'
+normal.limited = ['abcdef123456']
+normal.mab_only = True
