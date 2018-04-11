de43fed0440a20af0a7d3033bd750948a19ed06e
Sean Enck <enckse@gmail.com>
2018-04-11 11:03:47 -0400

    spec out trace output/debugging plugin

--- plugins/trace/plugin.go
+++ plugins/trace/plugin.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"github.com/epiphyte/radiucal/plugins"
+	"layeh.com/radius"
+)
+
+func Reload() {
+}
+
+func Setup(ctx *plugins.PluginContext) {
+
+}
+
+func Auth(packet *radius.Packet) bool {
+	return true
+}
+
+func Accounting(packet *radius.Packet) {
+}
