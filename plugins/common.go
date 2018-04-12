0b7343ef4faead545183707d588cab15e06d234a
Sean Enck <enckse@gmail.com>
2018-04-12 11:36:10 -0400

    better attempts at resolving types to names, even if I don't like it, would like upstream to do this but for now

--- plugins/common.go
+++ plugins/common.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/epiphyte/goutils"
 	"layeh.com/radius"
+	. "layeh.com/radius/rfc2865"
 	"os"
 	"path/filepath"
 	"plugin"
@@ -47,17 +48,44 @@ type Accounting interface {
 func KeyValueStrings(packet *radius.Packet) []string {
 	var datum []string
 	for t, a := range packet.Attributes {
-		datum = append(datum, fmt.Sprintf("Type: %d", t))
+		name := resolveType(t)
+		datum = append(datum, fmt.Sprintf("Type: %d (%s)", t, name))
 		for _, s := range a {
-			str := true
-			val := string(s)
-			for _, c := range val {
-				if !unicode.IsPrint(c) {
-					str = false
-					break
+			unknown := true
+			val := ""
+			i, err := radius.Integer(s)
+			if err == nil {
+				unknown = false
+				val = fmt.Sprintf("(int) %d", i)
+			}
+			if unknown {
+				ip, err := radius.IPAddr(s)
+				if err == nil {
+					unknown = false
+					val = fmt.Sprintf("(ip) %s", string(ip))
 				}
 			}
-			if !str {
+
+			if unknown {
+				d, err := radius.Date(s)
+				if err == nil {
+					unknown = false
+					val = fmt.Sprintf("(time) %s", d.Format(time.RFC3339))
+				}
+			}
+
+			if unknown {
+				val = string(s)
+				unknown = false
+				for _, c := range val {
+					if !unicode.IsPrint(c) {
+						unknown = true
+						break
+					}
+				}
+			}
+
+			if unknown {
 				val = fmt.Sprintf("(hex) %x", s)
 			}
 			datum = append(datum, fmt.Sprintf("Value: %s", val))
@@ -107,4 +135,92 @@ func LoadPlugin(path string, ctx *PluginContext) (Module, error) {
 	case Authing:
 		return t.(Authing), nil
 	}
+}
+
+func resolveType(t radius.Type) string {
+	switch t {
+	case UserName_Type:
+		return "User-Name"
+	case UserPassword_Type:
+		return "User-Password"
+	case CHAPPassword_Type:
+		return "CHAP-Password"
+	case NASIPAddress_Type:
+		return "NAS-IP-Address"
+	case NASPort_Type:
+		return "NAS-Port"
+	case ServiceType_Type:
+		return "Service-Type"
+	case FramedProtocol_Type:
+		return "Framed-Protocol"
+	case FramedIPAddress_Type:
+		return "Framed-IP-Address"
+	case FramedIPNetmask_Type:
+		return "Framed-IP-Netmask"
+	case FramedRouting_Type:
+		return "Framed-Routing"
+	case FilterID_Type:
+		return "Filter-ID"
+	case FramedMTU_Type:
+		return "Framed-MTU"
+	case FramedCompression_Type:
+		return "Framed-Compression"
+	case LoginIPHost_Type:
+		return "Login-IP-Host"
+	case LoginService_Type:
+		return "Login-Service"
+	case LoginTCPPort_Type:
+		return "Login-TCP-Port"
+	case ReplyMessage_Type:
+		return "Reply-Message"
+	case CallbackNumber_Type:
+		return "Callback-Number"
+	case CallbackID_Type:
+		return "Callback-ID"
+	case FramedRoute_Type:
+		return "Framed-Route"
+	case FramedIPXNetwork_Type:
+		return "Framed-IPX-Network"
+	case State_Type:
+		return "State"
+	case Class_Type:
+		return "Class"
+	case VendorSpecific_Type:
+		return "Vendor-Specific"
+	case SessionTimeout_Type:
+		return "Session-Timeout"
+	case IdleTimeout_Type:
+		return "Idle-Timeout"
+	case TerminationAction_Type:
+		return "Termination-Action"
+	case CalledStationID_Type:
+		return "Called-Station-ID"
+	case CallingStationID_Type:
+		return "Calling-Station-ID"
+	case NASIdentifier_Type:
+		return "NAS-Identifier"
+	case ProxyState_Type:
+		return "Proxy-State"
+	case LoginLATService_Type:
+		return "Login-LAT-Service"
+	case LoginLATNode_Type:
+		return "Login-LAT-Node"
+	case LoginLATGroup_Type:
+		return "Login-LAT-Group"
+	case FramedAppleTalkLink_Type:
+		return "Framed-Apple-Talk-Link"
+	case FramedAppleTalkNetwork_Type:
+		return "Framed-Apple-Talk-Network"
+	case FramedAppleTalkZone_Type:
+		return "Framed-Apple-Talk-Zone"
+	case CHAPChallenge_Type:
+		return "CHAP-Challenge"
+	case NASPortType_Type:
+		return "NAS-Port-Type"
+	case PortLimit_Type:
+		return "Port-Limit"
+	case LoginLATPort_Type:
+		return "Login-LAT-Port"
+	}
+	return "Unknown"
 }
