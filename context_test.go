4e889fc6442c24bf44cf3a185e797ab0eaac96b2
Sean Enck <enckse@gmail.com>
2018-04-14 11:09:18 -0400

    test in place and working

--- context_test.go
+++ context_test.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/epiphyte/radiucal/plugins"
+	"testing"
 	"layeh.com/radius"
 	"layeh.com/radius/rfc2865"
-	"testing"
+	"github.com/epiphyte/radiucal/plugins"
 )
 
 type MockModule struct {
@@ -48,30 +48,69 @@ func TestAuth(t *testing.T) {
 	ctx, p := getPacket(t)
 	m := &MockModule{}
 	ctx.auths = append(ctx.auths, m)
-	ctx.authorize(nil)
+	ctx.auth = true
+	// invalid packet
+	if !ctx.authorize(nil) {
+		t.Error("didn't authorize")
+	}
 	if m.auth != 0 {
 		t.Error("did auth")
 	}
-	ctx.authorize(p)
+	if !ctx.authorize(p) {
+		t.Error("didn't authorize")
+	}
 	if m.auth != 1 {
 		t.Error("didn't auth")
+	}
+	ctx.preauth = true
+	ctx.preauths = append(ctx.preauths, m)
+	if !ctx.authorize(p) {
+		t.Error("didn't authorize")
+	}
+	if m.auth != 2 {
+		t.Error("didn't auth again")
+	}
+	if m.pre != 1 {
+		t.Error("didn't preauth")
+	}
+	m.fail = true
+	if ctx.authorize(p) {
+		t.Error("did authorize")
+	}
+	if m.auth != 3 {
+		t.Error("didn't auth again")
+	}
+	if m.pre != 2 {
+		t.Error("didn't preauth")
+	}
+	ctx.auth = false
+	if ctx.authorize(p) {
+		t.Error("did authorize")
+	}
+	if m.auth != 3 {
+		t.Error("didn't auth again")
+	}
+	if m.pre != 3 {
+		t.Error("didn't preauth")
 	}
 }
 
 func getPacket(t *testing.T) (*context, []byte) {
-	var secret = []byte("secret")
-	p := radius.New(radius.CodeAccessRequest, secret)
-	if err := rfc2865.UserName_AddString(p, "user"); err != nil {
-		t.Error("unable to add user name")
-	}
-	if err := rfc2865.CallingStationID_AddString(p, "11-22-33-44-55-66"); err != nil {
-		t.Error("unable to add calling station")
+	c := &context{}
+	c.secret = "secret"
+    var secret = []byte(c.secret)
+    p := radius.New(radius.CodeAccessRequest, secret)
+    if err := rfc2865.UserName_AddString(p, "user"); err != nil {
+        t.Error("unable to add user name")
+    }
+    if err := rfc2865.CallingStationID_AddString(p, "11-22-33-44-55-66"); err != nil {
+        t.Error("unable to add calling station")
 	}
 	b, err := p.Encode()
 	if err != nil {
 		t.Error("unable to encode")
 	}
-	return &context{}, b
+	return c, b
 }
 
 func TestSecretParsing(t *testing.T) {
