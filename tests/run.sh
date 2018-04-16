83fc40a2f84f31f2f558f9fd806e320368592148
Sean Enck <enckse@gmail.com>
2018-04-16 09:52:59 -0400

    adding some more tests

--- tests/run.sh
+++ tests/run.sh
@@ -9,13 +9,20 @@ bin/harness
 sleep 1
 pkill radiucal
 pkill harness
+
+COMPARE="results stats"
 cat tests/log/radiucal.audit* | cut -d " " -f 2- > bin/results.log
-diff -u bin/results.log tests/expected.log
-if [ $? -ne 0 ]; then
-    echo "integration test failed"
-    exit 1
-fi
-echo "results match"
+cat tests/log/radiucal.stats.{auth,preauth}.* | grep -v -E "^(first|last)" > bin/stats.log
+
+for d in $(echo $COMPARE); do
+    diff -u bin/$d.log tests/expected.$d.log
+    if [ $? -ne 0 ]; then
+        echo "integration test failed ($d)"
+        exit 1
+    fi
+done
+
+echo "logged results match"
 if cat bin/count | grep -q "^count:4$"; then
     echo "count passes"
 else
